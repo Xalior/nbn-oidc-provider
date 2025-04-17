1fd1c78059d1afe85e663902b39e2dcc84903021
D. Rimron-Soutter <darran@xalior.com>
2025-04-17 12:02:35 +0100

    Fix error/race condition bug sending headers twice

--- src/controller/home.js
+++ src/controller/home.js
@@ -6,7 +6,7 @@ export default (app) => {
         if(req.user && req.session.destination_path) {
             const destinationPath = req.session.destination_path;
             delete(req.session.destination_path);
-            res.redirect(destinationPath);
+            return res.redirect(destinationPath);
         }
         return res.render('home');
     });
